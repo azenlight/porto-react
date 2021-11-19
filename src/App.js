1de0aed7581df8bf4b90e4c584bdbcd18f612fc8
azenlight <azenlightech@gmail.com>
2021-11-19 21:48:28 +0700

    Created Frame, Routing, Css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, useHistory, Link, Routes, Route, NavLink} from 'react-router-dom';
 import './App.css';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text">
+      &emsp;&emsp;I'm Az, a computer-science graduates that's aiming to become full stack Developer. My very first aim when
+      taking my major was to join GameDev Industry. But as the day goes, i realized demand on Web Development is higher
+      than GameDev. So i start to learn more about HTML, CSS, and mostly JavaScripts
+      <br/>
+      &emsp;&emsp;Compile-based Frameworks are on high demand right now. NodeJs, ReactJs, and VueJs are few of frameworks
+      that i tried to understand. Infact, you're looking at one ReactJS app now! I'm practicing, learning, and developing. 
+      I have huge excitement to develop and I surely hope to find team that will help me Grow!
     </div>
+  );
+}
+function Edu() {
+  return (
+    <div className="scrollable">
+      <div className="edu-card">
+        <div className="edu-header">
+          <h3>Computer Science Major (Indonesia)</h3>
+          <h4>(August 2017-August 2021)</h4>
+        </div>
+        <hr/>
+        <div className="edu-details">
+          GPA:3.63/4.00<br/>
+          Researched Initial Basic Feasible Solution for Transportation Problem(TP)
+          <br/>
+          <a>Transcript (Indonesian)</a>
+          <a></a>
+        </div>
+      </div>
+      <br/>
+      <div className="edu-card">
+        <div className="edu-header">
+          <h3>Senior High School (Indonesia)</h3>
+          <h4>(July 2014-July 2017)</h4>
+        </div>
+        <hr/>
+        <div className="edu-details">
+          Math and Science Minor
+        </div>
+      </div>
+    </div>
+    );
+}
+function Portofolios() {
+  return <h2>Portofolios</h2>;
+}
+function Skills() {
+  return <h2>Skills</h2>;
+}
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function App(props) {
+  return (
+    <Router>
+    <div className="App">
+      <div className="container table">
+        <div className="container-profile cell">
+          <div className="nama row">
+            {props.name}
+          </div>
+          <div className="profile row">
+            <img src={props.photoLink}></img>
+          </div>
+          <div className="icons row">
+            <a href={"https://github.com/"+props.github}><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img></a>
+            <a href={"mailto:"+props.email+"?subject=Portofolio%20React&body=Hello%20Az,"}><img src="https://cdn-icons-png.flaticon.com/512/1782/1782765.png"></img></a>
+          </div>
+        </div>
+        <div className="container-details cell">
+          <div className="navbar row">
+                
+                 <NavLink className="default" activeClassName="active" to="/" >Profile</NavLink> 
+                 <NavLink className="default" activeClassName="active" to="/Edu">Education</NavLink> 
+                 <NavLink className="default" activeClassName="active" to="/Portfolio">Creation</NavLink> 
+                 <NavLink className="default" activeClassName="active" to="/Ability">Skills</NavLink> 
+                 <NavLink className="default" activeClassName="active" to="/Contact">Contact</NavLink> 
+                
+            </div>
+          <div className="content row">
+              <Routes>
+                <Route path="/Edu" element={<Edu/>}/>
+                <Route path="/Portfolio" element={<Portofolios/>}/>
+                <Route path="/Ability" element={<Skills/>}/>
+                <Route path="/Contact" element={<Contact/>}/>
+                <Route path="/" element={<Home/>}/>
+              </Routes>
+          </div>
+        </div>
+      </div>
+    </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const profile={
+  name: "azenlight",
+  photoLink: "https://scontent.fcgk18-1.fna.fbcdn.net/v/t39.30808-6/217041259_1302707083460393_5880006625345283172_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_eui2=AeGQ1LDOt-rf5P9fAOWXIkVINo1G1SzOw_U2jUbVLM7D9VzWiGYQk6PuC2tvhlm5frhqbms_orqTqy4R5n4bFXxC&_nc_ohc=TuKhiCYhhBMAX-xbNmi&_nc_ht=scontent.fcgk18-1.fna&oh=ca40301b14d27f38e771693ebc82585b&oe=619B895A",
+  github:"azenlight",
+  email:"azenlightech@gmail.com",
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App {...profile}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
